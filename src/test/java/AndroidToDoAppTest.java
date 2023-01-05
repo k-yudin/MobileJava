741196b07423a763854815a7d66b6ddd53c179b1
k-yudin <telesens.yudin@gmail.com>
2023-01-05 16:50:24 +0200

    updated tests with parametrized data as json

--- src/test/java/AndroidToDoAppTest.java
+++ src/test/java/AndroidToDoAppTest.java
@@ -1,5 +1,11 @@
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import page_objects.CreateTaskPage;
+import page_objects.TasksListPage;
+import utils.JsonReader;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 
 public class AndroidToDoAppTest extends TestBase{
@@ -7,14 +13,21 @@ public class AndroidToDoAppTest extends TestBase{
     CreateTaskPage createTaskPage;
     TasksListPage tasksListPage;
 
-    @Test
-    public void testAddTask() throws MalformedURLException {
+    @DataProvider(name = "task data")
+    public Object[][] passData() throws IOException, ParseException {
+        return JsonReader.getJSONData(System.getProperty("user.dir") + "/data/TaskData.json",
+                "Tasks Data",
+                2);
+    }
+
+    @Test(dataProvider = "task data")
+    public void testAddTask(String taskName, String taskDescription) throws MalformedURLException {
         androidSetUp();
         tasksListPage = new TasksListPage(driver);
         createTaskPage = new CreateTaskPage(driver);
         tasksListPage.clickAddTaskButton();
-        createTaskPage.enterTaskName("Finish Appium");
-        createTaskPage.enterTaskDesc("Finishing Desc");
+        createTaskPage.enterTaskName(taskName);
+        createTaskPage.enterTaskDesc(taskDescription);
         driver.hideKeyboard();
         createTaskPage.clickSaveButton();
         tearDown();
