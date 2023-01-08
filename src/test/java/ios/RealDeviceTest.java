c71923aedea9a8f5c10cbebcd1e0265ace1e0b87
k-yudin <telesens.yudin@gmail.com>
2023-01-08 13:58:30 +0200

    updated mistyped driver class

--- src/test/java/ios/RealDeviceTest.java
+++ src/test/java/ios/RealDeviceTest.java
@@ -2,6 +2,7 @@ package ios;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -19,15 +20,15 @@ public class RealDeviceTest {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "iOS");
         caps.setCapability("automationName", "XCUITest");
-        caps.setCapability("platformVersion", "16.2");
-        caps.setCapability("deviceName", "iPhone 14 Pro");
+        caps.setCapability("platformVersion", "15.1");
+        caps.setCapability("deviceName", "Konstantin Yudin’s iPad");
         caps.setCapability("xcodeOrgId", "kyudin@mail.com");
         caps.setCapability("xcodeSigningId", "iPhone Developer");
         caps.setCapability("udid", "40b6b6bf3b1530f59d69f0ff3e80ae31f73d0dbf");
         caps.setCapability("appium:useNewWDA", true);
         caps.setCapability("appium:derivedDataPath", "/Users/kyudin/Library/Developer/Xcode/DerivedData/WebDriverAgent-eecgabsjhqcpozfkvfxzogkzcaly");
         caps.setCapability("app", "/Users/kyudin/Downloads/UIKitCatalog.app");
-        driver = new AndroidDriver(new URL("http://localhost:4723"), caps);
+        driver = new IOSDriver(new URL("http://localhost:4723"), caps);
     }
 
     @Test
